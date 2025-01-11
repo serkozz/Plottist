b7dc2434d8573522e2e416b4d5cfc05354a6095e
SergeyKozlov <serezha-kozlov.2002@mail.ru>
2025-01-11 16:41:05 +0400

    Добавлен контейнер зависимостей

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,71 @@ namespace Plottist
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public App()
+        {
+            // Creation of DI container
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            // Service provider creation
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        /// <summary>
+        /// Services registration method
+        /// </summary>
+        /// <param name="services"></param>
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // ViewModel registration
+            RegisterViewModels(services);
+
+            // Services registration
+            RegisterServices(services);
+
+            // Windows(views) registration
+            RegisterViews(services);
+
+            // Other components registration
+            RegisterOtherComponents(services);
+        }
+
+        private static void RegisterViewModels(IServiceCollection services)
+        {
+            // services.AddSingleton<MainViewModel>();
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            // services.AddSingleton<IProductService, ProductService>();
+        }
+
+        private static void RegisterViews(IServiceCollection services)
+        {
+            // services.AddSingleton<MainWindow>();
+        }
+
+        private static void RegisterOtherComponents(IServiceCollection services)
+        {
+            // services.AddSingleton<IOtherComponent, OtherComponent>();
+        }
+
+        /// <summary>
+        /// OnStartup overload for DI use on startup
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Get mainWindow from DI container
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+
+            // Show it
+            mainWindow.Show();
+        }
     }
 
 }
