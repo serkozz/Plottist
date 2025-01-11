cb95af7d9499eea632eda2b3d60b06c96164c94b
SergeyKozlov <serezha-kozlov.2002@mail.ru>
2025-01-11 16:29:58 +0400

    Добавлена базовая MVVM структура проекта

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Plottist.Commands
+{
+    public class RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null) : ICommand
+    {
+        private readonly Action<object?> _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        private readonly Func<object?, bool> _canExecute = canExecute ?? (_ => true);
+
+        public bool CanExecute(object? parameter) => _canExecute(parameter);
+
+        public void Execute(object? parameter) => _execute(parameter);
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Plottist
+
+![Commits Badge](https://img.shields.io/github/commit-activity/m/serkozz/Plottist?label=Total%20Commits)
