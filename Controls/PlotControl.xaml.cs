83720ff8862942b0d84d46f3d490eb67bd8bc6b1
SergeyKozlov <serezha-kozlov.2002@mail.ru>
2025-01-11 17:07:54 +0400

    Добавлен PlotControl (UserControl)

---
+++ Controls/PlotControl.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Plottist.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для PlotControl.xaml
+    /// </summary>
+    public partial class PlotControl : UserControl
+    {
+        public PlotControl()
+        {
+            InitializeComponent();
+        }
+
+        public void SetData(double[] xs, double[] ys)
+        {
+            plot.Plot.Clear();
+            plot.Plot.Add.Scatter(xs, ys);
+            plot.Refresh();
+        }
+    }
+}
